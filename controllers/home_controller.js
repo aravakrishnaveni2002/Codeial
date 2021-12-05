4e8b9a7509048798b3f4287796e1b54e1fd7de2c
ARAVA KRISHNAVENI <aravakrishnaveni@ARAVAs-MacBook-Air.local>
2021-12-05 14:57:26 +0530

    added view engine and views

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,9 @@
 module.exports.home = function(request,response){
 
-    return response.send('<h1>Express is up and running</h1>');
+    // return response.send('<h1>Express is up and running</h1>');
+
+    return response.render('home',{
+        title: 'Home'
+    });
 }
 
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,8 @@
 module.exports.profile = function(request,response){
 
-    return response.send('<h1>User Profile</h1>');
+    // return response.send('<h1>User Profile</h1>');
+
+    return response.render('user',{
+        title: 'User'
+    })
 }
