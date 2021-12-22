65b0cd0fd6751844e606b9deeef035b2324136d9
ARAVA KRISHNAVENI <aravakrishnaveni@ARAVAs-MacBook-Air.local>
2021-12-22 22:39:25 +0530

    added sign in and sign out

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,19 +1,55 @@
 
 const User = require('../models/user');
 
-module.exports.user = function(request,response){
+module.exports.profile = function(request,response){
 
     // return response.send('<h1>User Profile</h1>');
 
     //getting a cookie
-    // console.log(request.cookies);
+    console.log(request.cookies);
 
     //changing a cookie
     // response.cookie('user_id',24);
-    return response.render('user',{
-        title: 'User'
-    })
+
+    if(request.cookies.user_id == '0'){
+        return response.redirect('/user/sign-in');
+    }
+
+    else if(request.cookies.user_id){
+
+        console.log(request.cookies.user_id);
+
+        User.findOne({_id: request.cookies.user_id},function(err,user){
+            if(err){
+                console.log("Error in finding the user_id");
+                return;
+            }
+
+            if(!user){
+                return response.redirect('/user/sign-in');
+            }
+
+            
+            return response.render('profile',{
+                title: 'User',
+                user: user
+                        
+            });
+
+        });
+
+    }
+
+    else{
+        return response.redirect('/user/sign-in');
+    }
+
+    
 }
+
+// module.exports.profile = function(request,response){
+
+// }
 
 module.exports.signup = function(request,response){
     return response.render('signup',{
@@ -27,6 +63,11 @@ module.exports.signin = function(request,response){
     });   
 }
 
+module.exports.signout = function(request,response){
+    response.cookie('user_id','0');
+    return response.redirect('/');
+}
+
 module.exports.create = function(request,response){
 
     if(request.body.password != request.body.confrim_password){
@@ -35,14 +76,14 @@ module.exports.create = function(request,response){
 
     User.findOne({email: request.body.email},function(err,user){
         if(err){
-            console.log("Error in finding the user sigging up");
+            console.log("Error in finding the user signing up");
             return;
         }
 
         if(!user){
             User.create(request.body,function(err,user){
                 if(err){
-                    console.log("Error in creating the user sigging up");
+                    console.log("Error in creating the user signing up");
                     return;
                 }
 
@@ -59,5 +100,26 @@ module.exports.create = function(request,response){
 
 module.exports.createSession = function(request,response){
 
+    User.findOne({email: request.body.email},function(err,user){
+        if(err){
+            console.log("Error in finding the user signing in");
+            return; 
+        }
 
+        if(!user){
+            return response.redirect('back');
+        }
+
+        else{
+            if(request.body.password != user.password){
+                return response.redirect('back');
+            }
+
+            else{
+                response.cookie('user_id',user._id);
+                return response.redirect('/user/profile');
+            }
+
+        }
+    });
 }
--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,16 @@ const router = express.Router();
 
 const userController = require('../controllers/user_controller');
 
-// router.get('/profile',userController.profile);
- router.get('/',userController.user);
+router.get('/profile',userController.profile); 
 
 router.get('/sign-up',userController.signup);
 
 router.get('/sign-in',userController.signin);
 
+router.get('/sign-out',userController.signout);
+
 router.post('/create',userController.create);
+
+router.post('/create-session',userController.createSession);
 
 module.exports = router;
