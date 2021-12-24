263c4d05a5d32037967539c42e4dee155edcc56c
ARAVA KRISHNAVENI <aravakrishnaveni@ARAVAs-MacBook-Air.local>
2021-12-24 20:44:01 +0530

    added header and footer to all pages using express layouts

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 8000;
 const db = require('./config/mongoose');
 
+const expressLayouts = require('express-ejs-layouts');
 
 //used for seesion cookie
 const session = require('express-session');
@@ -20,7 +21,12 @@ app.use(cookieParser());
 
 
 
-app.use(express.static('assets'));
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+//extract styles and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 app.set('view engine','ejs');
 app.set('views','./views');
