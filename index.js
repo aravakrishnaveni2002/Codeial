3d0c650ec707c6833fc05f82a96dd2ee6aaf8419
ARAVA KRISHNAVENI <aravakrishnaveni@ARAVAs-MacBook-Air.local>
2021-12-24 21:45:29 +0530

    using SASS added scss to assets to  make the css part more easy to read

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-stratagy');
 const MongoStore = require('connect-mongo')(session);
 
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+
+
+
+}));
+
 
 
 app.use(express.urlencoded());
