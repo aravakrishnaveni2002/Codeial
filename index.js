cc368930804fe851c8a1bca9d9ee0fc07617db62
ARAVA KRISHNAVENI <aravakrishnaveni@ARAVAs-MacBook-Air.local>
2022-01-21 11:06:51 +0530

    added social authentication using passport-google-oauth and used crypto to generate random password

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-stratagy');
 const passportJWT = require('./config/passport-jwt-stratagy');
+const passportGoogle = require('./config/passport-google-oauth2-startegy');
 const MongoStore = require('connect-mongo')(session);
 
 const sassMiddleware = require('node-sass-middleware');
